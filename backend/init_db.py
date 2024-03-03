cef805dc95827ee498d06728e2a4c3121aa725e5
Sebastian Rodriguez <rodriguezseba2001@gmail.com>
2024-03-02 23:27:06 -0500

    Added logging for other tables

--- backend/init_db.py
+++ backend/init_db.py
@@ -6,8 +6,11 @@ def init_db():
     connection = sqlite3.connect('database.db')
     print("DB connection established!")
     with open('schema.sql') as f:
-        connection.executescript(f.read())        
+        connection.executescript(f.read())   
+             
     print("Users table set up!")
-    
+    print("event_hosts table set up!")
+    print("messages table set up!")
+
     connection.close()
 
