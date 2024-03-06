d8c7c07a31e95ee3a6f07062a2bfa59b47b9eeaa
Sebastian Rodriguez <Sebastian.Rodriguez@ice.com>
2024-03-06 15:14:38 -0500

    removed .venv folder.

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def simplify_event_data(event):
     simplified_event = {
         "name": event["name"],
@@ -10,21 +11,24 @@ def simplify_event_data(event):
             "name": event["_embedded"]["venues"][0]["name"],
             "city": event["_embedded"]["venues"][0]["city"]["name"],
             "state": event["_embedded"]["venues"][0]["state"]["name"],
-            "country": event["_embedded"]["venues"][0]["country"]["name"]
+            "country": event["_embedded"]["venues"][0]["country"]["name"],
         },
-        "image_url": event["images"][0]["url"] if event["images"] else None
+        "image_url": event["images"][0]["url"] if event["images"] else None,
     }
     return simplified_event
 
+
 def make_api_call(api_key):
-    url = f'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey={api_key}&size={10}&pages={1}'
-    
+    url = f"https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey={api_key}&size={10}&pages={1}"
+
     try:
         response = requests.get(url)
-        
+
         if response.status_code == 200:
             data = response.json()
-            simplified_events = [simplify_event_data(event) for event in data["_embedded"]["events"]]
+            simplified_events = [
+                simplify_event_data(event) for event in data["_embedded"]["events"]
+            ]
             return simplified_events
         else:
             print(f"Failed to fetch data. Status code: {response.status_code}")
@@ -33,11 +37,12 @@ def make_api_call(api_key):
         print(f"An error occurred: {e}")
         return None
 
-api_key = 'a7ZZ3PEKgvGkg54s5nr6j6qKg9QdVcvW'
+
+api_key = "a7ZZ3PEKgvGkg54s5nr6j6qKg9QdVcvW"
 simplified_data = make_api_call(api_key)
 if simplified_data:
     for i in range(len(simplified_data)):
         print(f"{i}: {simplified_data[i]}\n")
-        
+
 else:
     print("Failed to fetch API data.")
--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,16 @@
-
 from flask import Flask
 from init_db import init_db
 from auth.user.views import auth
 from auth.event_manager.views import event_manager_auth
 from event_manager.views import event_manager
 from event_guest.views import event_guest
+import os
 
+key = os.getenv("API_KEY")
 
+print(key)
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'
+app.secret_key = "your_secret_key_here"
 app.register_blueprint(auth)
 app.register_blueprint(event_manager_auth)
 app.register_blueprint(event_manager)
@@ -17,5 +19,5 @@ app.register_blueprint(event_guest)
 init_db()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
