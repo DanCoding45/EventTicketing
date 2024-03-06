37d08018e1dedb65b9c27b82e34e1159d45b21b7
Sebastian Rodriguez <Sebastian.Rodriguez@ice.com>
2024-03-06 15:16:55 -0500

    removed mistake

--- backend/app.py
+++ backend/app.py
@@ -4,11 +4,8 @@ from auth.user.views import auth
 from auth.event_manager.views import event_manager_auth
 from event_manager.views import event_manager
 from event_guest.views import event_guest
-import os
 
-key = os.getenv("API_KEY")
 
-print(key)
 app = Flask(__name__)
 app.secret_key = "your_secret_key_here"
 app.register_blueprint(auth)
