e1c3c9657e2b36b1fb10552099c63989ab019ba8
Jay Limbasia <jaylimbasia25@gmail.com>
2024-03-12 12:09:24 -0400

    implemented service function to filter events table

--- backend/event_guest/services/event_access_services.py
+++ backend/event_guest/services/event_access_services.py
@@ -1,16 +1,22 @@
 import sqlite3
 
 class EventAccessServices:
-    
-    def get_events(self):
+        
+    def get_events(self, category=None):
         
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
         
-        query = "SELECT * FROM events LIMIT 10"
-        cursor.execute(query)
+        if category:
+            print(category)
+            query = "SELECT * FROM events WHERE category = ?"
+            cursor.execute(query, (category, ))
+        else:
+            query = "SELECT * FROM events LIMIT 10"
+            cursor.execute(query)
         
-        events = cursor.fetchall()        
+        events = cursor.fetchall()
+        print(events)        
         conn.close()
         return events
         
