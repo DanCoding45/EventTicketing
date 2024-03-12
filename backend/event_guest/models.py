7bf24bff61e1697feba9cebebf4984f32b2fa76b
Jay Limbasia <jaylimbasia25@gmail.com>
2024-03-12 12:08:48 -0400

    updated event model

--- backend/event_guest/models.py
+++ backend/event_guest/models.py
@@ -2,11 +2,14 @@
 
 class Event:
     
-    def __init__(self, id, name, date, time, city, country, venue_name):
+    def __init__(self, id, name, category, date, time, city, state, country, venue_name, image_url):
         self.id = id
         self.name = name
+        self.category = category
         self.date = date
         self.time = time
         self.city = city
+        self.state = state
         self.country = country
         self.venue_name = venue_name
+        self.image_url = image_url
