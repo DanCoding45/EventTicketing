221130ce05560c601db1e74fb0cb40a295bed813
Jay Limbasia <jaylimbasia25@gmail.com>
2024-04-06 11:53:32 -0400

    removed unused forms

--- backend/shopping_cart/forms.py
+++ backend/shopping_cart/forms.py
@@ -6,8 +6,4 @@ class AddToCartForm(FlaskForm):
     quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=1, message="Quantity must be at least 1")], render_kw={'type': 'number'})
     submit = SubmitField('Add to Cart')
 
-class RemoveFromCartForm(FlaskForm):
-    submit = SubmitField('Remove')
 
-class ClearCartForm(FlaskForm):
-    submit = SubmitField('Clear Cart')
